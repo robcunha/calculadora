77380382b28ca2a80cc6e6d3d64da23e43f0bcda
Roberto Cunha <roberto@MBP-de-usuario.box>
2022-01-05 01:50:08 -0300

    Adicionar número do display

---
+++ components/display.js
@@ -0,0 +1,9 @@
+import styles from '../styles/components/display.module.sass'
+
+export default function Display({ ac }) {
+  return (
+    <div className={styles.display}>
+      <p>0</p>
+    </div>
+  )
+}
